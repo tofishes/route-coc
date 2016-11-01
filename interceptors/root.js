86084ba55021e1fafd65c13bc4c81875a29523ed
bodhi <bodhi@lianj.com>
2016-11-01 23:39:27 +0800

    add interceptor config example

---
+++ interceptors/root.js
@@ -0,0 +1,11 @@
+module.exports = {
+  '/*': {
+    'api': 'http://113.108.139.178:9190/user/getUserInfo',
+    'series': true,
+    'name': 'userInfo',
+    handle(data, req, res) {
+      res.forward('/proxy');
+      return data;
+    }
+  }
+};
