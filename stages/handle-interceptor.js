09959eccb5ef116600089f4a6d8122fd27923303
bodhi <bodhi@lianj.com>
2016-11-15 22:46:10 +0800

    update exclude ext static file

--- stages/handle-interceptor.js
+++ stages/handle-interceptor.js
@@ -3,13 +3,21 @@ const handleConfig = require('./handle-router').handleConfig;
  * 处理拦截器配置
  * 拦截器有两种：一种预处理，一种并处理
  * 拦截器处理在匹配router后进行，可以得到参数
+ * 拦截器需排除静态资源，根据是否包含点符号
  * @param  {[type]}   req  [description]
  * @param  {[type]}   res  [description]
  * @param  {Function} next [description]
  * @return {[type]}        [description]
  */
 function match(pathname, interceptors) {
-  return interceptors.filter(interceptor => interceptor.pathRegx.exec(pathname));
+  return interceptors.filter(interceptor => {
+    const route = interceptor.route;
+    const matchPath = interceptor.pathRegx.exec(pathname);
+    const routeExt = ~route.indexOf('.');
+    const ignoreExt = routeExt || !~pathname.indexOf('.');
+
+    return matchPath && ignoreExt;
+  });
 }
 function handleInterceptor(req, res, next) {
   const pathname = req.pathname;
