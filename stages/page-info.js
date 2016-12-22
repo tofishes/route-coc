c4c2bb461a8bf62605d1caf2e233c5aea054a291
bodhi <bodhi@lianj.com>
2016-12-22 11:55:00 +0800

    update default modulename

--- stages/page-info.js
+++ stages/page-info.js
@@ -1,5 +1,6 @@
 const parser = require('ua-parser-js');
 const slash = '/';
+const defaultModuleName = 'home';
 /**
  * 页面变量初始化等
  * @param  {[type]}   req  [description]
@@ -11,7 +12,7 @@ module.exports = function pageInfo(req, res, next) {
   const pathname = req.pathname.replace(slash, '');
   const firstSlash = pathname.indexOf(slash);
 
-  const moduleName = pathname.substr(0, firstSlash);
+  const moduleName = pathname.substr(0, firstSlash) || defaultModuleName;
   const ua = parser(req.get('User-Agent'));
 
   Object.assign(req, {
