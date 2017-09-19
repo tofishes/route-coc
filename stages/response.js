30845659753d6f5270791d0b6ef9171bfc7854e3
bodhi <bodhi@lianj.com>
2017-09-19 18:20:11 +0800

    fix proxy xhr disable cache

--- stages/response.js
+++ stages/response.js
@@ -1,4 +1,9 @@
 function response(req, res, next) {
+  const disablePageCache = req.router && req.router.pageCache === false;
+  const disableAjaxCache = req.xhr && this.get('ajaxCache') === false;
+
+  res.disableCache(disablePageCache || disableAjaxCache);
+
   if (res.proxyResoponse) {
     return res.proxyResoponse.pipe(res);
   }
@@ -9,16 +14,10 @@ function response(req, res, next) {
    * 若想ajax始终得到json，不要配置view
    */
   if (res.html) {
-    const disablePageCache = req.router && req.router.pageCache === false;
-    res.disableCache(disablePageCache);
-
     return res.send(res.html);
   }
 
   if (req.xhr) {
-    const disableAjaxCache = req.xhr && this.get('ajaxCache') === false;
-    res.disableCache(disableAjaxCache);
-
     return res.json(res.apiData);
   }
 
